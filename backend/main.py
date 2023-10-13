56bc98d100e748ce434463e5f7c0525d030b46ac
Albin Vass <albin.vass@gmail.com>
2023-10-13 14:53:18 +0200

    Mongodb api

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
+import pymongo
 from fastapi import FastAPI
+from pydantic import BaseModel
+mongo_client = pymongo.MongoClient("mongodb",
+                                   username="splitfree",
+                                   password="splitfree",
+                                   authSource="splitfree")
 
 app = FastAPI()
 
@@ -6,4 +12,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class Expense(BaseModel):
+    name: str
+    price: float
+
+@app.post("/expense")
+async def expense(expense: Expense):
+    mongo_client.splitfree.expenses.insert_one(expense.dict())
+    return expense
 
