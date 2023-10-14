006a4559cfe268f0cb5e13961419353eb34c9eb1
Albin Vass <albin.vass@gmail.com>
2023-10-14 14:41:23 +0200

    Add image download

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ import uuid
 from typing import Dict
 from typing_extensions import Annotated
 from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import StreamingResponse
 from pydantic import (
     BaseModel,
     UUID4,
@@ -15,8 +16,10 @@ mongo_client = pymongo.MongoClient("mongodb",
                                    username="splitfree",
                                    password="splitfree",
                                    authSource="splitfree")
+
+minio_endpoint_url = "http://minio:9000"
 s3 = boto3.client('s3',
-    endpoint_url="http://minio:9000",
+    endpoint_url=minio_endpoint_url,
     aws_access_key_id="minioadmin",
     aws_secret_access_key="minioadmin",
 )
@@ -67,12 +70,23 @@ async def expense(e: Expense):
     return expense
 
 @app.post("/api/receipt")
-async def receipt(expense_id: ExpenseId = Form(...), file: UploadFile = File(...)):
-    print(dir(expense_id))
+async def post_receipt(expense_id: ExpenseId = Form(...), file: UploadFile = File(...)):
+    expense_query = {"expense_id": str(expense_id)}
+    mongo_client.splitfree.expenses.find_one(expense_query)
     try:
         s3.upload_fileobj(file.file, 'splitfree', str(expense_id))
     except Exception as e:
         print(e)
+    receipt_url = f"{minio_endpoint_url}/splitfree/{expense_id}}}"
+    mongo_client.splitfree.expenses.update_one(
+        expense_query,
+        { "$set": {"receipt_url": receipt_url} }
+    )
 
-    return {"receipt_url": "receipt_url"}
-
+@app.get("/api/receipt/{expense_id}")
+async def get_receipt(expense_id: ExpenseId):
+    try:
+        result = s3.get_object(Bucket="splitfree", Key=str(expense_id))
+        return StreamingResponse(result["Body"].iter_chunks())
+    except Exception as e:
+        print(e)
